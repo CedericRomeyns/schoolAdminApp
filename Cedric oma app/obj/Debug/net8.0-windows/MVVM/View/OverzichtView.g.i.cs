72c4ee964b79d8ba0359b8d21f8791e99baee9fa
CedericRomeyns <romeynscederic1@gmail.com>
2024-08-01 00:59:17 +0200

    ..

--- Cedric oma app/obj/Debug/net8.0-windows/MVVM/View/OverzichtView.g.i.cs
+++ Cedric oma app/obj/Debug/net8.0-windows/MVVM/View/OverzichtView.g.i.cs
@@ -47,13 +47,13 @@ namespace Cedric_oma_app.MVVM.View {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Cedric oma app;V1.0.0.0;component/mvvm/view/overzichtview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Cedric oma app;component/mvvm/view/overzichtview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\MVVM\View\OverzichtView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -63,7 +63,7 @@ namespace Cedric_oma_app.MVVM.View {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
